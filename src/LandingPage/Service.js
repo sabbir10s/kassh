3b51034ccb4f9df6af4a6046e3b5ddabbb1b89d4
sabbir10s <sabbirahmed1023@gmail.com>
2022-09-23 17:20:49 +0600

    service card design done

--- src/LandingPage/Service.js
+++ src/LandingPage/Service.js
@@ -2,42 +2,54 @@ import React from 'react';
 
 const Service = () => {
     return (
-        <div className='bg-white '>
-            <div className='backdrop-blur bg-white/20 py-20'>
-                <div className='container mx-auto'>
-                    <div className='text-center'>
-                        <h1 className='text-orange-500 text-5xl font-bold capitalize'>Services customized to your need</h1>
-                        <p className='my-3 text-xl font-bold'>Light + Sustainability + Technology</p>
-                    </div>
-                    <div className='pt-10 '>
-                        <div className='grid grid-cols-2 items-center  bgImage2'>
-                            <div></div>
-                            <div className='backdrop-blur bg-white/50 h-full text-center py-48'>
-                                <h2 className='text-4xl font-bold text-orange-500'>HUMAN-CENTRIC LIGHTING</h2>
-                                <p className='font-bold text-xl my-3'>Designing with light for people, their space and its architecture</p>
+        <div className='bgImage3'>
+            <div className='container mx-auto'>
+                <div className='text-center pt-14'>
+                    <h1 className='text-5xl font-bold capitalize text-white'>Services customized to your need</h1>
+                    <p className='my-3 text-xl font-bold text-orange-500'>Light + Sustainability + Technology</p>
+                </div>
+                <div className='grid grid-cols-3 gap-10 py-14'>
+                    <div className='h-[400px] backdrop-blur bg-white/20 p-5 rounded-lg shadow-lg shadow-orange-300 flex justify-center items-center'>
+                        <div className='grid grid-rows-2'>
+                            <div>
+                                <img className='w-24 rounded-full mx-auto' src="https://www.glamox.com/globalassets/pbs/human-centric-lighting/light-and-human-biology-05.jpg?width=480&height=480&focalpoint=(0,0,0,0),(0.5,0.5,1,1)&format=jpg" alt="" />
+                                <h2 className=' text-2xl font-bold text-white py-2 text-center'>HUMAN-CENTRIC LIGHTING</h2>
+                            </div>
+                            <div>
+
+                                <p className='text-black font-bold my-3 text-sm text-center'>Designing with light for people, their space and its architecture</p>
                             </div>
                         </div>
-                        <div className='grid grid-cols-2 mt-10'>
-                            <div className='flex justify-center items-center h-[500px] bgImage3 '>
-                                <div className='backdrop-blur bg-white/50 mx-10 py-24 px-4'>
-                                    <h2 className=' text-4xl font-bold text-orange-500'>SUSTAINABILITY</h2>
-                                    <p className='text-black font-bold my-3'>Reducing LIGHT CARBON of cities and buildings with our sustainable solutions to create a healthier planet and its ecosystem.</p>
-                                    <p>Know your green potential</p>
-                                </div>
+                    </div>
+                    <div className='h-[400px] backdrop-blur bg-white/20 p-5 rounded-lg shadow-lg shadow-orange-300 flex justify-center items-center '>
+                        <div className='grid grid-rows-2'>
+                            <div>
+                                <img className='w-24 rounded-full mx-auto bg-white' src="https://velcdn.azureedge.net/-/media/marketing/sustainability/icons/14-share-sustainable-innovations.svg" alt="" />
+                                <h2 className=' text-2xl font-bold text-white py-2 text-center'>SUSTAINABILITY</h2>
                             </div>
+                            <div className='text-center'>
 
-                            <div className='flex justify-center items-center h-[500px] bgImage4'>
-                                <div className='backdrop-blur bg-white/50 mx-10 py-24 px-4'>
-                                    <h2 className=' text-4xl font-bold text-orange-500'>TECHNOLOGY</h2>
-                                    <p className='text-black font-bold my-3'>Maximising potential of  LIGHT TECHNOLOGY with inclusive designs to build future proof smart infrastructure.</p>
-                                    <p>Explore your solutions</p>
-                                </div>
+                                <p className='text-black font-bold my-3 text-sm'>Reducing LIGHT CARBON of cities and buildings with our sustainable solutions to create a healthier planet and its ecosystem.</p>
+                                <p className='font-bold hover:text-white cursor-pointer'>Know your green potential</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='h-[400px] backdrop-blur bg-white/20 p-5 rounded-lg shadow-lg shadow-orange-300  flex justify-center items-center'>
+                        <div className='grid grid-rows-2'>
+                            <div>
+                                <img className='w-24 rounded-full mx-auto' src="https://static.vecteezy.com/system/resources/previews/005/893/682/original/technology-innovation-line-icon-creativity-solution-concept-light-bulb-and-gear-linear-pictogram-lightbulb-and-cog-wheel-outline-icon-isolated-illustration-vector.jpg" alt="" />
+                                <h2 className=' text-2xl font-bold text-white py-2 text-center'>TECHNOLOGY</h2>
+                            </div>
+                            <div className='text-center'>
+
+                                <p className='text-black font-bold my-3 text-sm'>Maximising potential of  LIGHT TECHNOLOGY with inclusive designs to build future proof smart infrastructure.</p>
+                                <p className='font-bold hover:text-white cursor-pointer'>Explore your solutions</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
